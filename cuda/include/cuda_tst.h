739506a99a398be61ec65266203e05713e3096e3
Aldas25 <aldas.lenksas@gmail.com>
2026-05-06 11:59:00 +0200

    extern C in case of cpp

--- cuda/include/cuda_tst.h
+++ cuda/include/cuda_tst.h
@@ -1,6 +1,14 @@
 #ifndef CUDA_TEST_H
 #define CUDA_TEST_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void vectorAdd(const float* a, const float* b, float* c, int n);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CUDA_TEST_H
