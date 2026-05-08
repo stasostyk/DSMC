6b08972a58e3eedf443f96b25afa53e9ce9a054a
Aldas25 <aldas.lenksas@gmail.com>
2026-05-08 11:54:42 +0200

    compilatino fix

--- cuda/src/collider.cu
+++ cuda/src/collider.cu
@@ -82,8 +82,8 @@ __global__ void no_time_counter_scheme_kernel(
                 double collisionProb = d_conf.ntcs_collisionProbMultiplier * sqrt(relativeSpeed);
                 if (curand_uniform(&rngState) < collisionProb) {
                     elastic_collision( 
-                        &vx[i], &vy[i], &vz[i], 
-                        &vx[j], &vy[j], &vz[j],
+                        &(d_particleData.vx[i]), &(d_particleData.vy[i]), &(d_particleData.vz[i]), 
+                        &(d_particleData.vx[j]), &(d_particleData.vy[j]), &(d_particleData.vz[j]),
                         relativeSpeed, &rngState );
                     collisions++;
                 }
