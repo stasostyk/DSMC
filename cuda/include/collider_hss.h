28b87625534848618f58f463de6bfdb0c2b1169b
Stas Ostyk <s.ostyks@gmail.com>
2026-05-18 15:31:02 +0200

    initial naive implementation of HSS algorithm (inshallah works)

---
+++ cuda/include/collider_hss.h
@@ -0,0 +1,8 @@
+//
+// Created by sosty on 18/05/2026.
+//
+
+#ifndef DSMC_COLLIDER_HSS_CUH
+#define DSMC_COLLIDER_HSS_CUH
+
+#endif //DSMC_COLLIDER_HSS_CUH
---
+++ cuda/src/collider_hss.cu
@@ -0,0 +1,5 @@
+//
+// Created by sosty on 18/05/2026.
+//
+
+#include "collider_hss.cuh"
