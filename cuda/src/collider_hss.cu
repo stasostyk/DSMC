1d0db52a3f1fc23360f312eece6a938b16f958aa
Stas Ostyk <s.ostyks@gmail.com>
2026-05-18 23:03:44 +0200

    hybrid parameter test

--- cuda/src/collider_hss.cu
+++ cuda/src/collider_hss.cu
@@ -45,7 +45,7 @@ __global__ void hss_scheme_kernel(unsigned long long *total_collisions,
 
 
 
-        if (NPC < 400) return;
+        if (NPC < 300) return;
 
         int rngIdx = IDX_CELL(blockIdx.x, blockIdx.y, blockIdx.z) * blockDim.x * blockDim.y * blockDim.z + tid;
         curandState rngState = rngStates[rngIdx];
