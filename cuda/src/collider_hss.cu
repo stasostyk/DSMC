939b33a22bc258abecaacccc06e8506023976bb5
Stas Ostyk <s.ostyks@gmail.com>
2026-05-24 23:34:47 +0200

    test

--- cuda/src/collider_hss.cu
+++ cuda/src/collider_hss.cu
@@ -14,7 +14,7 @@ __global__ void hss_scheme_kernel(unsigned long long *total_collisions,
                                   int *cellCount,
                                   int *cellList,
                                   curandState *rngStates) {
-    __shared__ int collisionsBlock[256];
+    __shared__ int collisionsBlock[1];
     __shared__ int localParticleList[MAX_PARTICLES_PER_CELL];
     __shared__ int NPC;
     __shared__ int N_x;
@@ -123,7 +123,7 @@ __global__ void hss_scheme_kernel(unsigned long long *total_collisions,
 }
 
 void collide_particles_hss(Simulation *sim) {
-    dim3 threadsPerBlock(256);
+    dim3 threadsPerBlock(1);
     dim3 blocksPerGrid(NX, NY, NZ);
 
     hss_scheme_kernel<<<blocksPerGrid, threadsPerBlock>>>(
