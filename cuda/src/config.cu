8611b83cc991b03a8dc7bb1c485a21eb4926f321
Stas Ostyk <s.ostyks@gmail.com>
2026-05-25 15:59:08 +0200

    dynamic parallelism FAIL

--- cuda/src/config.cu
+++ cuda/src/config.cu
@@ -91,7 +91,7 @@ void config_setup(Config *config) {
     config->ntcs_collisionProbMultiplier = pow(config->CrRef, config->ntcs_collisionProbExponent) * config->sigmaRef / majorant;
 
     config->hss_nbatch = 3;
-    config->hss_threshold = 0.0; // threshold for switching to HSS, in number of particles per cell
+    config->hss_threshold = 400.0; // threshold for switching to HSS, in number of particles per cell
     config->hss_collisionProbMultiplier = sqrt(config->CrRef) * (1.0/config->hss_nbatch) * config->weight * config->sigmaRef * config->dt / config->cellVolume;
 
     config->generation_derivatedMultiplier = sqrt(config->KB * config->TFree / config->moleculeMass);
