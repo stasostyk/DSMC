432e4ca9eedcee3c2ec130ea9d4d02f448c7418a
Aldas25 <aldas.lenksas@gmail.com>
2026-06-04 00:17:36 +0200

    fixed compilation

--- cuda/include/mpi_exchange.h
+++ cuda/include/mpi_exchange.h
@@ -1,6 +1,9 @@
 #ifndef MPI_EXCHANGE_H
 #define MPI_EXCHANGE_H
 
+#include "mpi_helper.h"
+#include "simulation.h"
+
 void exchange_boundary_particles(Simulation *sim, MPIHelper *mpiHelper);
 
 #endif
